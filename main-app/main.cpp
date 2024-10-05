dcc5267edc49f12bd51bf176ff6590aba9f7eed1
SomeRandomGuy45 <dragonlionmusic@gmail.com>
2024-10-04 22:26:44 -0500

    updated main.cpp for stuff

--- main-app/main.cpp
+++ main-app/main.cpp
@@ -1,8 +1,15 @@
 #include "generator.h"
 #include "upload.h"
+#include <vector>
+
+std::vector<std::string> args;
 
 int main(int argc, char **argv)
 {
+    for (int i = 0; i < argc; i++)
+    {
+        args.push_back(argv[i]);
+    }
     if (argc > 1)
     {
         std::string command(argv[1]);
@@ -11,7 +18,7 @@ int main(int argc, char **argv)
             std::string newProject = CreateNewProject();
             UploadZip(newProject);
         }
-        else if (command == "publish")
+        else if (command == "publish" || command == "update" || command == "upd")
         {
             if (!std::filesystem::exists(std::filesystem::current_path().string() + "/project.toml")) return 0;
             UploadZip(std::filesystem::current_path().string());
@@ -22,6 +29,6 @@ int main(int argc, char **argv)
     std::cout << "Usage: simp [command] [options]" << std::endl;
     std::cout << "Commands:" << std::endl;
     std::cout << " init, create, innit: Create's a new project" << std::endl;
-    std::cout << " publish: Publish the current project" << std::endl;
+    std::cout << " publish, upd, update: Publish or updates the current project" << std::endl;
     return 0;
 }
